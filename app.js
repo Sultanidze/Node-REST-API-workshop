43ad4e2cec3241c3bfcaba625882d44601b07011
Sultanidze <Sultanidze@ukr.net>
2020-04-28 17:32:00 +0300

    fix lint errors and warnings

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+// eslint-disable-next-line no-unused-vars
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
@@ -18,5 +19,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log('Running on port ', port);
 });
